97ccf2c119abb71928cbcb1783dabbd97c7c9d42
Ian O'Malley <omalleyian18@gmail.com>
2017-07-14 21:48:39 -0500

    Added possible test nodes to WaveNet Comparison Chart

--- python_scripts/ercot_neighbors_plotting.py
+++ python_scripts/ercot_neighbors_plotting.py
@@ -7,7 +7,7 @@ sys.path.append("/home/omalleyian/Documents/energy_market_project/scripts")
 from ercot_data_interface import ercot_data_interface
 
 raw = []
-with open('neighbor_zeroes.csv', 'rb') as csvFile:
+with open('removed_duplicates.csv', 'rb') as csvFile:
     reader = csv.reader(csvFile, dialect='excel')
     for row in reader:
         raw.append(row)
@@ -16,21 +16,21 @@ table = []
 for row in raw:
     table.append([row[0], ast.literal_eval(row[1])])
 
-possible = []
-for group in table:
-    over = False
-    for i in group[1]:
-        if i > 10000:
-            over = True
-    if not over:
-        row = [group[0], group[1]]
-        possible.append(row)
+# possible = []
+# for group in table:
+#     over = False
+#     for i in group[1]:
+#         if i > 10000:
+#             over = True
+#     if not over:
+#         row = [group[0], group[1]]
+#         possible.append(row)
 
 ercot = ercot_data_interface(password='Is79t5Is79t5')
 
+center = table[len(table) - 53][0]
 
-center = possible[3][0]
-nn = ercot.get_nearest_CRR_neighbors('SIL_SILAS_6')
+nn = ercot.get_nearest_CRR_neighbors(center)
 for n in nn:
     plt.plot(ercot.query_prices(n, '2011-01-01', '2016-5-23'), label=n)
 plt.legend()
